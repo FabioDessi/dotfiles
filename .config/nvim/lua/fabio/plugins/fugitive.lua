55af130076e0a923a54147a1db343bd108669d5a
Fabio Dessi <f.dessi@fabiodessi.dev>
2024-09-20 20:21:17 +0200

    nvim: remove old config

--- .config/nvim/lua/fabio/plugins/fugitive.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "tpope/vim-fugitive",
-  config = function()
-    vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-    vim.keymap.set("n", "<leader>dgj", "<cmd>diffget //2<CR>")
-    vim.keymap.set("n", "<leader>dgk", "<cmd>diffget //3<CR>")
-    vim.keymap.set("n", "<leader>dg", "<cmd>diffput<CR>")
-  end
-}
--- .config/nvim/lua/fabio/remap.lua
+++
@@ -1,4 +0,0 @@
-vim.g.mapleader = ' '
-vim.keymap.set('i', 'jj', '<ESC>', { silent = true })
-vim.keymap.set('n', '<leader>bn', ':bnext<CR>', { silent = true })
-vim.keymap.set('n', '<leader>bp', ':bprevious<CR>', { silent = true })
