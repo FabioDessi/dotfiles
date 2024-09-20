0f2f7b237191db606639660bf75c7201d7949b4f
Fabio Dessi <f.dessi@fabiodessi.dev>
2024-09-20 20:29:49 +0200

    nvim: adjust diffget keymaps

--- .config/nvim/lua/fabioMKII/plugins/fugitive.lua
+++ .config/nvim/lua/fabioMKII/plugins/fugitive.lua
@@ -2,8 +2,8 @@ return {
   "tpope/vim-fugitive",
   config = function()
     vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-    vim.keymap.set("n", "<leader>dgj", "<cmd>diffget //2<CR>")
-    vim.keymap.set("n", "<leader>dgk", "<cmd>diffget //3<CR>")
+    vim.keymap.set("n", "<leader>dgh", "<cmd>diffget //2<CR>")
+    vim.keymap.set("n", "<leader>dgl", "<cmd>diffget //3<CR>")
     vim.keymap.set("n", "<leader>dg", "<cmd>diffput<CR>")
   end
 }
