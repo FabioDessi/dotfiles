cbca847d280701929522afeed5f8194c84cc3407
Fabio Dessi <f.dessi@fabiodessi.dev>
2024-09-21 21:38:01 +0200

    nvim: adjust noice config

--- .config/nvim/lua/fabioMKII/plugins/noice.lua
+++ .config/nvim/lua/fabioMKII/plugins/noice.lua
@@ -2,6 +2,7 @@ return {
   "folke/noice.nvim",
   event = "VeryLazy",
   opts = {},
+
   dependencies = {
     -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
     -- "MunifTanjim/nui.nvim",
@@ -13,28 +14,30 @@ return {
 
   config = function()
     require("noice").setup({
+      -- FIX: Code actions window bocked
       lsp = {
         hover = {
           silent = true,
         },
+
         documentation = {
           view = "hover",
+
           opts = {
             lang = "markdown",
             replace = true,
             render = "plain",
             format = { "{message}" },
             position = { row = 2, col = 2 },
-            size = {
-              -- max_width = 0.9 * vim.api.nvim_win_get_width(0),
-              -- max_height = 15,
-            },
+
             border = {
               style = "rounded",
             },
+
             win_options = {
               concealcursor = "n",
               conceallevel = 3,
+
               winhighlight = {
                 Normal = "CmpPmenu",
                 FloatBorder = "DiagnosticSignInfo",
@@ -43,20 +46,12 @@ return {
           },
         },
 
-        override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-          ["vim.lsp.util.show_code_action_menu"] = true
-        },
-
-        presets = {
-          bottom_search = true,         -- use a classic bottom cmdline for search
-          command_palette = true,       -- position the cmdline and popupmenu together
-          long_message_to_split = true, -- long messages will be sent to a split
-          inc_rename = false,           -- enables an input dialog for inc-rename.nvim
-          lsp_doc_border = false,       -- add a border to hover docs and signature help
-        }
+        --   presets = {
+        --     bottom_search = true,         -- use a classic bottom cmdline for search
+        --     command_palette = true,       -- position the cmdline and popupmenu together
+        --     long_message_to_split = true, -- long messages will be sent to a split
+        --     inc_rename = false,           -- enables an input dialog for inc-rename.nvim
+        --   }
       },
     })
   end
