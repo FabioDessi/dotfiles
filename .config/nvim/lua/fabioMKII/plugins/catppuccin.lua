8028f0fc62c1b3b3ebdb1c0218809751094feb08
Fabio Dessi <f.dessi@fabiodessi.dev>
2024-10-03 02:56:32 +0200

    nvim: update catppuccin config

--- .config/nvim/lua/fabioMKII/plugins/catppuccin.lua
+++ .config/nvim/lua/fabioMKII/plugins/catppuccin.lua
@@ -5,12 +5,31 @@ return {
 
   config = function()
     require("catppuccin").setup({
+      highlight_overrides = {
+        mocha = function(mocha)
+          return {
+            LineNr = { fg = mocha.text },
+          }
+        end
+      },
+
       flavour = "mocha",
 
       integrations = {
         notify = true,
         treesitter = true,
         cmp = true,
+        gitsigns = true,
+        noice = true,
+
+        indent_blankline = {
+          enabled = true,
+          colored_indent_levels = false
+        },
+
+        native_lsp = {
+          enabled = true
+        }
       }
     })
 
