7ba6a8e23c2b496cbcd59cf9f07952a50fb37631
Fabio Dessi <f.dessi@fabiodessi.dev>
2024-10-03 02:35:23 +0200

    nvim: rework indent-blankline config

--- .config/nvim/lua/fabioMKII/plugins/indent-blankline.lua
+++ .config/nvim/lua/fabioMKII/plugins/indent-blankline.lua
@@ -2,20 +2,34 @@ return {
   'lukas-reineke/indent-blankline.nvim',
   main = "ibl",
 
-  opts = {
-    indent = { highlight = highlight, char = "|" },
-    whitespace = {
-      highlight = {
-        "CursorColumn",
-        "Whitespace",
+  config = function()
+    local hooks = require("ibl.hooks")
+    local highlight = {
+      "Whitespace",
+      "CursorColumn",
+    }
+
+    vim.opt.list = true
+    hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
+    hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_tab_indent_level)
+
+
+    require("ibl").setup({
+      indent = {
+        char = "▏",
       },
-      remove_blankline_trail = false,
-    },
 
-    scope = { enabled = true, show_exact_scope = true },
-  },
+      whitespace = {
+        highlight = highlight,
+        remove_blankline_trail = true,
+      },
 
-  config = function(_, opts)
-    require('ibl').setup(opts)
+      scope = {
+        enabled = true,
+        show_exact_scope = true,
+        show_start = false,
+        show_end = false
+      },
+    })
   end
 }
