55015be7e4428ae48cbc7f17d09f5650cee80962
Fabio Dessi <f.dessi@fabiodessi.dev>
2024-11-05 20:00:01 +0100

    nvim: blankline config

--- .config/nvim/lua/fabioMKII/plugins/indent-blankline.lua
+++ .config/nvim/lua/fabioMKII/plugins/indent-blankline.lua
@@ -3,31 +3,18 @@ return {
   main = "ibl",
 
   config = function()
-    local hooks = require("ibl.hooks")
-    local highlight = {
-      "Whitespace",
-      "CursorColumn",
-    }
-
-    vim.opt.list = true
-    hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
-    hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_tab_indent_level)
-
     require("ibl").setup({
       indent = {
         char = "▏",
       },
 
       whitespace = {
-        highlight = highlight,
         remove_blankline_trail = true,
       },
 
       scope = {
         enabled = true,
         show_exact_scope = true,
-        show_start = true,
-        show_end = true,
       },
     })
   end,
--- .config/nvim/lua/fabioMKII/plugins/lualine.lua
+++ .config/nvim/lua/fabioMKII/plugins/lualine.lua
@@ -1,33 +1,32 @@
 return {
   "nvim-lualine/lualine.nvim",
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  dependencies = { "nvim-tree/nvim-web-devicons" },
 
-  config = function ()
+  config = function()
     require("lualine").setup({
       options = {
-        theme = 'catppuccin',
-        section_separators = {'', ''},
-        component_separators = {'', ''},
+        theme = "catppuccin",
+        section_separators = { "", "" },
+        component_separators = { "", "" },
       },
       sections = {
-        lualine_a = {'mode'},
-        lualine_b = {'branch'},
-        lualine_c = {'filename'},
-        lualine_x = {'encoding', 'fileformat', 'filetype'},
-        lualine_y = {'progress'},
-        lualine_z = {'location'}
+        lualine_a = { "mode" },
+        lualine_b = { "branch" },
+        lualine_c = { "filename" },
+        lualine_x = { "encoding", "fileformat", "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
       },
       inactive_sections = {
         lualine_a = {},
         lualine_b = {},
-        lualine_c = {'filename'},
-        lualine_x = {'location'},
+        lualine_c = { "filename" },
+        lualine_x = { "location" },
         lualine_y = {},
-        lualine_z = {}
+        lualine_z = {},
       },
       tabline = {},
-      extensions = {}
+      extensions = {},
     })
-  end
+  end,
 }
-
