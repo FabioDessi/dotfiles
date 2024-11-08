a3fe3a5873be69a4528661acc285c109f35dbfa1
Fabio Dessi <f.dessi@fabiodessi.dev>
2024-11-08 17:54:33 +0100

    nvim: add nvim-tree keymap

--- .config/nvim/lua/fabioMKII/plugins/nvim-tree.lua
+++ .config/nvim/lua/fabioMKII/plugins/nvim-tree.lua
@@ -12,6 +12,9 @@ return {
       },
     })
 
-    vim.keymap.set("n", "<leader>n", ":NvimTreeFocus<CR>", { noremap = true, silent = true })
+    vim.keymap.set("n", "<leader>n", ":NvimTreeFindFile<CR>", { noremap = true, silent = true })
+    vim.keymap.set("n", "<leader>N", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
+    vim.keymap.set("n", "<leader>nf", ":NvimTreeFocus<CR>", { noremap = true, silent = true })
+    vim.keymap.set("n", "<leader>nc", ":NvimTreeCollapse<CR>", { noremap = true, silent = true })
   end,
 }
